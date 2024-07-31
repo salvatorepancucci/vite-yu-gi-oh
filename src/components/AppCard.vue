a62b4ce47e084929facbc715ecd75f31a0cae46c
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-31 15:29:02 +0200

    Card images classes binded and added along with info to AppCard.vue template

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,7 +8,15 @@ export default {
 </script>
 
 <template>
- 
+ <div class="col-md-3 mb-3">
+    <div class="card h-100">
+      <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
+      <div class="card-body">
+        <h5 class="card-title">{{ card.name }}</h5>
+        <p class="card-text">{{ card.type }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
