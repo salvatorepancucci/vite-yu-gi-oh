159cdd0d2ed5bfc0bb9aff0d7c89f6a44e9848b5
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-31 15:11:45 +0200

    Header added and styled

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,23 @@
 <script>
-
+    export default {
+        name: 'AppHeader'
+    }
 </script>
 
 <template>
- 
+    <header>
+        <h2> Yu-Gi-Oh</h2>
+    </header>
 </template>
+  
 
 <style lang="scss" scoped>
 @import './styles/generals.scss';
 
+header {
+    background-color: white;
+    color: black;
+    padding: 5px;
+    height: 50px;
+}
 </style>
