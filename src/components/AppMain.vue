e35fad9be6cd55610fca58035947a079784a11f3
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-31 15:21:16 +0200

    Imported Axios + chosen API called through axios on AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,24 @@
 <script>
-import AppCard from './AppCard.vue';
+import axios from 'axios'
+import AppCard from './AppCard.vue'
 
+export default {
+  name: 'AppMain',
+  components: {
+    AppCard
+  },
+  data() {
+    return {
+      cards: []
+    }
+  },
+  created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then(response => {
+        this.cards = response.data.data
+      })
+  }
+}
 </script>
 
 <template>
